8160058613e2650cdd2bc46ab18ba6fba6d0b633
MrRespect454 <iv.bogi367@gmail.com>
2025-11-20 14:18:00 +0700

    created a category entity

---
+++ pathfinder/src/main/java/ru/pathfinder/domain/Category.java
@@ -0,0 +1,18 @@
+package ru.pathfinder.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    private String iconUrl;
+}
