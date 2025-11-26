5152ebe73e98aa5036e069e6a09ce3410d7aad2d
MrRespect454 <iv.bogi@gmail.com>
2025-11-27 04:24:15 +0700

    Fix: Fixed validation errors by simplifying spatial queries. Updated InterestingtagService to use simplified search on first launch. Template resolution has been adjusted. Logs have been added for controllers and template rendering.

--- pathfinder/src/main/java/ru/pathfinder/web/WebController.java
+++ pathfinder/src/main/java/ru/pathfinder/web/WebController.java
@@ -3,30 +3,37 @@ package ru.pathfinder.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 
 public class WebController {
     @GetMapping("/")
     public String index()   
     {
+        log.info("Loading index page");
         return "index"; 
     }      
     
     @GetMapping("/map")
     public String map() 
     {
+        log.info("Loading map page");
         return "map";
     }
 
     @GetMapping("/register")
     public String register() 
     {
+        log.info("Loading register page");
         return "register"; 
     }
     
     @GetMapping("/login")
     public String login()
      {
+        log.info("Loading login page");
         return "login"; 
     }
     
