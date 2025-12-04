e1466c26181a308bdc7308431dd3ece6ae4f49c5
Kirill Alei <aleykirill@gmail.com>
2025-12-04 14:54:22 +0700

    now you can jump to /, but app still starts with /login

--- pathfinder/src/main/java/ru/pathfinder/PathfinderApplication.java
+++ pathfinder/src/main/java/ru/pathfinder/PathfinderApplication.java
@@ -2,9 +2,9 @@ package ru.pathfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class PathfinderApplication {
@@ -13,8 +13,9 @@ public class PathfinderApplication {
         SpringApplication.run(PathfinderApplication.class, args);
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    // Уже есть в SecurityConfig
+    //@Bean
+    //public PasswordEncoder passwordEncoder() {
+    //    return new BCryptPasswordEncoder();
+    //}
 }
